1d80963b22fd0e6ad71f51e61589afeb7f638f7f
Sergev Solovyov <sergey.ss.solovyov@gmail.com>
2020-09-12 15:05:38 +0300

    functions names were improved

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,7 +1,7 @@
 import startGameEngine from '../index.js';
 import makeRandomNumber from '../calculators/random-number-calculator.js';
 
-const findGCD = (num1, num2) => {
+const findGcd = (num1, num2) => {
   // a = bq + r, where 0 <= r < | b |
   let a = num1;
   let b = num2;
@@ -16,19 +16,19 @@ const findGCD = (num1, num2) => {
   return b;
 };
 
-const makePairOfNumbers = () => {
+const makeNumbersAndGcd = () => {
   const multipliers = [2, 3, 4, 5, 6, 7, 8, 9];
   const { length } = multipliers;
   const num1 = makeRandomNumber(2, 10) * multipliers[makeRandomNumber(0, length - 1)];
   const num2 = makeRandomNumber(2, 10) * multipliers[makeRandomNumber(0, length - 1)];
-  const gcd = String(findGCD(num1, num2));
+  const gcd = String(findGcd(num1, num2));
   return [num1, num2, gcd];
 };
 
 export const makeQuestionsAnswers = (counter) => {
   const iter = (acc) => {
     if (acc.length === counter) return acc;
-    const [num1, num2, answer] = makePairOfNumbers();
+    const [num1, num2, answer] = makeNumbersAndGcd();
     const question = `${num1} ${num2}`;
     return iter([...acc, [question, answer]]);
   };
